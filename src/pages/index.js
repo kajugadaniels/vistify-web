e22ce763e1259ae0952df0c6587a4e6a6df30950
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-01 12:05:02 +0200

    Setup food menu page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Explore } from './Explore'
 export { default as PlaceDetails } from './PlaceDetails'
+export { default as PlaceFoodMenu } from './PlaceFoodMenu'
 export { default as NotFound } from './NotFound'
