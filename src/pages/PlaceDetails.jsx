6a910694f5ae5fa0aba85ebb3670baa4a7c78131
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-01 10:30:35 +0200

    Setup place details page

--- src/pages/PlaceDetails.jsx
+++ src/pages/PlaceDetails.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const PlaceDetails = () => {
     return (
-        <div>PlaceDetails</div>
+        <>
+            <div className="breadcrumbs">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6 col-md-6 col-12">
+                            <div className="breadcrumbs-content">
+                                <h1 className="page-title">Place</h1>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-6 col-12">
+                            <ul className="breadcrumb-nav">
+                                <li><a href="#"><i className="lni lni-home"></i> Explore</a></li>
+                                <li>Place</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
