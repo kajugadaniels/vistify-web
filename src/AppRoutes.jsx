99ce5ffe5a025a2d4721070d524366e55e15ee95
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-01 12:06:29 +0200

    Setup route for food menu

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import AppLayout from './layouts/AppLayout'
-import { Explore, NotFound, PlaceDetails } from './pages'
+import { Explore, NotFound, PlaceDetails, PlaceFoodMenu } from './pages'
 
 const AppRoutes = () => {
     return (
@@ -9,6 +9,7 @@ const AppRoutes = () => {
             <Route element={<AppLayout />}>
                 <Route path="/" element={<Explore />} />
                 <Route path="/place/:id/" element={<PlaceDetails />} />
+                <Route path="/place/:id/menu/" element={<PlaceFoodMenu />} />
             </Route>
 
             <Route path='*' element={<NotFound />} />
