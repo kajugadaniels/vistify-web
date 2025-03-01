e7d45290401ebed870f848fc05256c7089bfa4d4
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-01 10:28:15 +0200

    Design sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <div className="product-sidebar">
+            <div className="single-widget search">
+                <h3>Search Place</h3>
+                <form action="#">
+                    <input type="text" placeholder="Search Here..." />
+                    <button type="submit"><i className="lni lni-search-alt"></i></button>
+                </form>
+            </div>
+            <div className="single-widget">
+                <h3>All Categories</h3>
+                <ul className="list">
+                    <li>
+                        <a href="#">Bar </a><span>(1138)</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
